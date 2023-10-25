a32e27c173f6250934023b66d01759df93d63567
Chan Yik Wai <yichan@student.42kl.edu.my>
2023-10-25 16:31:15 +0800

    a

---
+++ ex01/srcs/RPN.cpp
@@ -0,0 +1,40 @@
+#include "RPN.hpp"
+
+int	RPN(std::string equation)
+{
+	std::stack<int> RPN;
+	std::string token;
+	std::istringstream insert(equation);
+
+	while (insert >> token)
+	{
+		std::cout << ORANGE << token << RESET << std::endl;
+		if (token.length() != 1)
+			throw std::runtime_error("Error"); // runtime_error being thrown due to the wrong format being input
+		else if (isdigit(token[0]))
+			RPN.push(atoi(token.c_str()));
+		else{ // when the token[0] is not digit, checking for "+-*/ "
+			if (RPN.empty())
+				throw std::runtime_error("Error");
+			int lower_elem = RPN.top();
+			RPN.pop();
+
+			if (RPN.empty())
+				throw std::runtime_error("Error");
+			int upper_elem = RPN.top();
+			RPN.pop();
+
+			if (token[0] == '+')
+				RPN.push(upper_elem + lower_elem);
+			else if (token[0] == '-')
+				RPN.push(upper_elem - lower_elem);
+			else if (token[0] == '*')
+				RPN.push(upper_elem * lower_elem);
+			else if (token[0] == '/')
+				RPN.push(upper_elem / lower_elem);
+			else if (token[0] != ' ')
+				throw std::runtime_error("Error"); //anything other than given symbol / digits should be error handled
+		}
+	}
+	return RPN.top();
+}
